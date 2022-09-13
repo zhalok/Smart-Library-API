5e8386bcfd24c4a4130e43ee1916d3630cea2c3c
zhalok <zhalok>
2022-09-13 21:37:17 +0600

    .

--- routes/history-route.js
+++ routes/history-route.js
@@ -3,6 +3,6 @@ const route = express.Router();
 const book_controller = require("../controllers/book-controller");
 
 route.get("/read", user_controller.authorize, book_controller.view_recent);
-route.get("/search", user_controller.authorize, book_controller.view_recent);
+route.get("/search", user_controller.authorize, book_controller.search_history);
 
 module.exports = route;
